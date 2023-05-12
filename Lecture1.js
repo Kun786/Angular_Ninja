e00866277a4599c72032162f71b101b0baa1964d
fazi1live <faziislive@gmail.com>
2023-05-13 02:36:22 +0500

    add

---
+++ Lecture1.js
@@ -0,0 +1,9 @@
+// Understanding Destructing
+const myArray = [10,20,30]
+const [firstValue,...rest] = myArray
+console.log(firstValue);
+console.log(rest);
+// Understanding Destructing
+
+let x;
+console.log(typeof(undefined))
